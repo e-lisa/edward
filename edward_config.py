40c37ab30be7fab2c12efeadceabc709ce3dcb50
Andrew Engelbrecht <sudoman@ninthfloor.org>
2015-07-19 16:13:19 -0400

    use a settings file to manage configurations

---
+++ edward_config.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""*********************************************************************
+* Edward is free software: you can redistribute it and/or modify       *
+* it under the terms of the GNU Affero Public License as published by  *
+* the Free Software Foundation, either version 3 of the License, or    *
+* (at your option) any later version.                                  *
+*                                                                      *
+* Edward is distributed in the hope that it will be useful,            *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+* GNU Affero Public License for more details.                          *
+*                                                                      *
+* You should have received a copy of the GNU Affero Public License     *
+* along with Edward.  If not, see <http://www.gnu.org/licenses/>.      *
+*                                                                      *
+* Copyright (C) 2015 Andrew Engelbrecht                     (AGPLv3+)  *
+* Copyright (C) 2014 Josh Drake                             (AGPLv3+)  *
+* Copyright (C) 2014 Lisa Marie Maginnis                    (AGPLv3+)  *
+************************************************************************
+
+Code sourced from these projects:
+
+  * http://agpl.fsf.org/emailselfdefense.fsf.org/edward/CURRENT/edward.tar.gz
+
+"""
+
+gnupghome = "/home/e/edward/.gnupg/"
+sign_with_key = "F357AA1A5B1FA42CFD9FE52A9FF2194CC09A61E8"
+
