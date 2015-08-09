01ceaec37a3a0263b2ebf753cfc314d2d4a0de1e
Andrew Engelbrecht <sudoman@ninthfloor.org>
2015-08-09 17:41:31 -0400

    added spanish and italian translations
    these were in the old edward install, in /home/e/edward

---
+++ lang/es.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""*********************************************************************
+* This file is part of Edward the GPG Bot.                             *
+*                                                                      *
+* Edward is free software: you can redistribute it and/or modify       *
+* it under the terms of the GNU Affero Public License as published by  *
+* the Free Software Foundation, either version 3 of the License, or    *
+* (at your option) any later version.                                  *
+*                                                                      *
+* Edward is distributed in the hope that it will be useful,            *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+* GNU Affero Public License for more details.                          *
+*                                                                      *
+* You should have received a copy of the GNU Affero Public License     *
+* along with Edward.  If not, see <http://www.gnu.org/licenses/>.      *
+*                                                                      *
+* Josh Drake, Copyright (c) 2014                                       *
+* Lisa Marie Maginnis, Copyright (c) 2014                              *
+*********************************************************************"""
+
+replies = {
+    'success_decrypt' : "He recibido tu mensaje y lo he descifrado. Aquí tienes una copia de tu mensaje:",
+    'public_key_received' : 'Hola, soy Edward, el simpático robot GnuPG. He recibido tu clave pública. Gracias.',
+    'failed_decrypt' : "Lo siento, no he podido descifrar tu mensaje. ¿Estás seguro de haberlo cifrado con mi clave pública?",
+    'no_public_key' : "Lo siento, no he podido encontrar tu clave pública. ¿Te acordaste de adjuntarla?",
+    'sig_failure' : 'No he podido verificar tu firma criptográfica.',
+    'sig_success' : 'He verificado con éxito tu firma criptográfica.',
+    'signature' : '- Edward, el simpático robot GnuPG\n\nMe creó la Free Software Foundation. ¿Puedes hacer una donación para apoyar su trabajo? | https://www.fsf.org/donate'
+}
+
---
+++ lang/it.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""*********************************************************************
+* This file is part of Edward the GPG Bot.                             *
+*                                                                      *
+* Edward is free software: you can redistribute it and/or modify       *
+* it under the terms of the GNU Affero Public License as published by  *
+* the Free Software Foundation, either version 3 of the License, or    *
+* (at your option) any later version.                                  *
+*                                                                      *
+* Edward is distributed in the hope that it will be useful,            *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+* GNU Affero Public License for more details.                          *
+*                                                                      *
+* You should have received a copy of the GNU Affero Public License     *
+* along with Edward.  If not, see <http://www.gnu.org/licenses/>.      *
+*                                                                      *
+* Josh Drake, Copyright (c) 2014                                       *
+* Lisa Marie Maginnis, Copyright (c) 2014                              *
+*********************************************************************"""
+
+replies = {
+    'success_decrypt' : "Ciao, io sono Edward, l'amichevole bot GnuPG. Ho ricevuto il tuo messaggio e l'ho decriptato. Questa è una copia del tuo messaggio:",
+    'public_key_received' : 'Ho ricevuto la tua chiave pubblica. Grazie.',
+    'failed_decrypt' : "Mi dispiace, non sono stato in grado di decifrare il tuo messaggio. Sei sicuro di averlo criptato con la mia chiave pubblica?",
+    'no_public_key' : "Mi dispiace, non ho trovato la tua chiave pubblica. Ti sei ricordato di allegarla?",
+    'sig_failure' : 'La tua firma non può essere verificata.',
+    'sig_success' : 'La tua firma è stata verificata.',
+    'signature' : "- Edward, l'amichevole bot GnuPG\n\nLa Free Software Foundation mi ha creato. Potresti fare una donazione per supportare il loro lavoro? | https://www.fsf.org/donate"
+}
+
